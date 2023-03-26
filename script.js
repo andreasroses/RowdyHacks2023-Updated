14e1d6559335369684cd7200c0dcdb40cb297e9b
Andrea Mendez <andreacelestem11@gmail.com>
2023-03-26 11:15:16 -0500

    finally reading inputs

--- script.js
+++ script.js
@@ -2,19 +2,27 @@ function hover(img)
 {
     img.src = "2.jpg"
 }
-function getOutputDate(dateobj){
-    var date = new Date();
-    var dateobj = date.toISOString().substring(0,10);
-    document.getElementById('dateobj').value = dateobj;
-    return dateobj;
+function remove(nuke){
+    nuke.remove();
 }
-
-function getMoonPhase(dob){
-    const bday = document.getElementById('dob').value;
+// function newBoxes(dob){
+//     //const newNode = document.createElement("div");
+//     const textNode = document.createTextNode(getZodiacSign(dob));
+//     document.getElementById("header").append(textNode);
+// }
+function callFunctions(a,b,c){
+    console.log(a,b,c);
+    getMoonPhase(a,b);
+    getZodiacSign(a,c);
+}
+function getMoonPhase(dob,mppng){
     const data = null;
+   // console.log("Moon Phase Requested!");
+    var bDay = input.value;
+   // console.log(bDay);
     var mp;
-    const sp = bday.split("/");
-    const bob = sp[2]+"-"+sp[0]+"-"+sp[1];
+    const sp = bDay.split("-");
+    const bob = sp[0]+"-"+sp[1]+"-"+sp[2];
     var date = "https://moon-calendar.p.rapidapi.com/moon_phase?date=";
     date = date.concat(bob);
 const xhr = new XMLHttpRequest();
@@ -24,54 +32,56 @@ xhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
         console.log(JSON.parse(this.response).moon_phase);
 		mp = JSON.parse(this.response).moon_phase;
+        var mppng = document.getElementById('merge');
+console.log("This is MP "+mp);
+if(mp =='First Quarter'){
+    mppng.src="lunarphase/first-quarter.png";
+}
+if(mp == 'Full Moon'){
+    mppng.src="lunarphase/full.png";
+}
+if(mp == 'New Moon'){
+    console.log("true");
+    mppng.src='lunarphase/new-moon.png';
+}
+if(mp == 'Third Quarter'){
+    mppng.src='lunarphase/third-quarter.png';
+}
+if(mp == 'Waxing Gibbous'){
+    mppng.src='lunarphase/waxing-gibbous.png';
+}
+
+if(mp == 'Waning Gibbous'){
+    mppng.src='lunarphase/waning-gibbous.png';
+}
+if(mp == 'Waning Crescent'){
+    mppng.src='lunarphase/waning-crescent.png';
+}
+if(mp == 'Waxing Crescent'){
+    mppng.src='lunarphase/waxing-crescent.png';
+}
 	}
-});
+}
+);
 
 xhr.open("GET", date);
 xhr.setRequestHeader("X-RapidAPI-Key", "e756fb4d03msh17e5a6b0398e7a4p163ee7jsn08863d810795");
 xhr.setRequestHeader("X-RapidAPI-Host", "moon-calendar.p.rapidapi.com");
 
 xhr.send(data);
-
-if(mp =='First Quarter'){
-    mp = 'first-quarter';
-}
-if(mp == 'Full Moon'){
-    mp = 'full';
-}
-if(mp == 'New Moon'){
-    mp =  'new-moon';
-}
-if(mp == 'Third Quarter'){
-    mp =  'third-quarter';
-}
-if(mp == 'Waxing Gibbous'){
-    mp = 'waxing-gibbous';
 }
 
-if(mp == 'Waning Gibbous'){
-    mp =  'waning-gibbous';
-}
-if(mp == 'Waning Crescent'){
-    mp =  'waning-crescent';
-}
-if(mp == 'Waxing Crescent'){
-    mp =  'waxing-crescent';
-}
-console.log(mp);
-return mp;
-}
-
-function getZodiacSign(dob){
-    const bday = document.getElementById('dob').value;
+function getZodiacSign(input,txtBox){
+    getMoonPhase(input);
+    console.log("Zodiac Sign Requested!");
+    var bDay = input.value;
+    console.log(bDay);
     const data = null;
     var final;
     var date = "https://horoskopos.p.rapidapi.com/zodiac-signs/search?day=";
-    const sp = bday.split("/");
-    const bob = sp[2]+"-"+sp[0]+"-"+sp[1];
-    console.log(bob);
-    console.log(sp);
-    date = date.concat(sp[1],"&lang=en&month=",sp[0],"&year=",sp[2],"&date=",bob);
+    const sp = bDay.split("-");
+    const bob = sp[0]+"-"+sp[1]+"-"+sp[2];
+    date = date.concat(sp[2],"&lang=en&month=",sp[1],"&year=",sp[0],"&date=",bob);
     const options = {
 	method: 'GET',
 	headers: {
@@ -81,12 +91,8 @@ function getZodiacSign(dob){
 };
 
 fetch(date, options)
-	.then((response) => response.json())
-	.then((data) => {
-        console.log(data.zodiacSign.name);
-        final = console.log(data.zodiacSign.name);
-        })
-	    .catch(err => console.error(err));
+.then(response => response.json())
+.then(txtBox.innerHTML = response)
+	  //  .catch(err => console.error(err));
 
-    return final;
 }
