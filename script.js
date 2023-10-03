997b146f9ed03ee019e490704aeffaa864d81343
andreasroses <andreacelestem11@gmail.com>
2023-10-02 22:58:38 -0500

    new API implemented

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-function hover(img)
-{
+function hover(img) {
     img.src = "2.jpg"
 }
-function remove(nuke){
+function remove(nuke) {
     nuke.remove();
 }
 // function newBoxes(dob){
@@ -10,75 +9,81 @@ function remove(nuke){
 //     const textNode = document.createTextNode(getZodiacSign(dob));
 //     document.getElementById("header").append(textNode);
 // }
-function callFunctions(a,b,c){
-    if(!a.value || !b.value){
+function callFunctions(a, b, c, d) {
+    alert('Your moons are now merging for your compatibility');
+    if (!a.value || !b.value) {
         window.alert('Missing date!');
     }
-    else{
-    getMoonPhase(a,c);
-    getMoonPhase(b,c);
-    getZodiacSign(a);
-    getZodiacSign(b);
-}
+    else {
+        getMoonPhase(a, c);
+        getMoonPhase(b, d);
+        // getZodiacSign(a);
+        // getZodiacSign(b);
+    }
     //getZodiacSign(b,c);
 }
-function getMoonPhase(dob,mppng){
+function getMoonPhase(dob, id) {
     const data = null;
-   // console.log("Moon Phase Requested!");
+    console.log("Moon Phase Requested!");
     var bDay = dob.value;
-   console.log('dob',dob);
-   //document.getElementById(dob.id).parentNode.parentNode.innerHTML=''; //visibility = 'hidden';
     var mp;
-    const sp = bDay.split("-");
-    const bob = sp[0]+"-"+sp[1]+"-"+sp[2];
-    var date = "https://moon-calendar.p.rapidapi.com/moon_phase?date=";
-    date = date.concat(bob);
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+    var date = "https://aa.usno.navy.mil/api/rstt/oneday?date=" + bDay + "&coords=40.927643, -98.338205";
+    var mppng;
+    fetch(date)
+        .then(response => {
+            if (response.ok) {
+                return response.json(); // Parse the response data as JSON
+            } else {
+                throw new Error('API request failed');
+            }
+        })
+        .then(data => {
+            // Process the response data here
+            console.log(data);
+            if (data.hasOwnProperty('curphase')) {
+                mp = data.curphase;
+            }
+            else {
+                mp = data.closestphase.phase;
+            }
+            console.log("This is MP " + mp);
+            if (mp == 'First Quarter') {
+                mppng = "lunarphase/first-quarter.png";
+            }
+            if (mp == 'Full Moon') {
+                mppng = "lunarphase/full.png";
+            }
+            if (mp == 'New Moon') {
+                console.log("true");
+                mppng = 'lunarphase/new-moon.png';
+            }
+            if (mp == 'Last Quarter') {
+                mppng = 'lunarphase/third-quarter.png';
+            }
+            if (mp == 'Waxing Gibbous') {
+                mppng = 'lunarphase/waxing-gibbous.png';
+            }
 
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-       // console.log(JSON.parse(this.response).moon_phase);
-		mp = JSON.parse(this.response).moon_phase;
-console.log("This is MP "+mp);
-if(mp =='First Quarter'){
-    mppng.src="lunarphase/first-quarter.png";
-}
-if(mp == 'Full Moon'){
-    mppng.src="lunarphase/full.png";
-}
-if(mp == 'New Moon'){
-    console.log("true");
-    mppng.src='lunarphase/new-moon.png';
-}
-if(mp == 'Third Quarter'){
-    mppng.src='lunarphase/third-quarter.png';
-}
-if(mp == 'Waxing Gibbous'){
-    mppng.src='lunarphase/waxing-gibbous.png';
+            if (mp == 'Waning Gibbous') {
+                mppng = 'lunarphase/waning-gibbous.png';
+            }
+            if (mp == 'Waning Crescent') {
+                mppng = 'lunarphase/waning-crescent.png';
+            }
+            if (mp == 'Waxing Crescent') {
+                mppng = 'lunarphase/waxing-crescent.png';
+            } // Example: Logging the data to the console
+            console.log(mp);
+            const moonImg = document.getElementById(id);
+            moonImg.src = mppng;
+        })
+        .catch(error => {
+            // Handle any errors here
+            console.error(error); // Example: Logging the error to the console
+        });
 }
 
-if(mp == 'Waning Gibbous'){
-    mppng.src='lunarphase/waning-gibbous.png';
-}
-if(mp == 'Waning Crescent'){
-    mppng.src='lunarphase/waning-crescent.png';
-}
-if(mp == 'Waxing Crescent'){
-    mppng.src='lunarphase/waxing-crescent.png';
-}
-	}
-}
-);
-
-xhr.open("GET", date);
-xhr.setRequestHeader("X-RapidAPI-Key", "e756fb4d03msh17e5a6b0398e7a4p163ee7jsn08863d810795");
-xhr.setRequestHeader("X-RapidAPI-Host", "moon-calendar.p.rapidapi.com");
-
-xhr.send(data);
-}
-
-function getZodiacSign(input){
+function getZodiacSign(input) {
     console.log("Zodiac Sign Requested!");
     var bDay = input.value;
     console.log(bDay);
@@ -86,18 +91,18 @@ function getZodiacSign(input){
     var final;
     var date = "https://horoskopos.p.rapidapi.com/zodiac-signs/search?day=";
     const sp = bDay.split("-");
-    const bob = sp[0]+"-"+sp[1]+"-"+sp[2];
-    date = date.concat(sp[2],"&lang=en&month=",sp[1],"&year=",sp[0],"&date=",bob);
+    const bob = sp[0] + "-" + sp[1] + "-" + sp[2];
+    date = date.concat(sp[2], "&lang=en&month=", sp[1], "&year=", sp[0], "&date=", bob);
     const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'e756fb4d03msh17e5a6b0398e7a4p163ee7jsn08863d810795',
-		'X-RapidAPI-Host': 'horoskopos.p.rapidapi.com'
-	}
-};
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'e756fb4d03msh17e5a6b0398e7a4p163ee7jsn08863d810795',
+            'X-RapidAPI-Host': 'horoskopos.p.rapidapi.com'
+        }
+    };
 
-fetch(date, options)
-.then(response => response.json())
-.then(data => {console.log(data.zodiacSign.name);  document.getElementById(input.id).parentNode.parentNode.innerHTML="Zodiac Sign: " + data.zodiacSign.name;})
-.catch(err => console.error(err));
+    fetch(date, options)
+        .then(response => response.json())
+        .then(data => { console.log(data.zodiacSign.name); document.getElementById(input.id).parentNode.parentNode.innerHTML = "Zodiac Sign: " + data.zodiacSign.name; })
+        .catch(err => console.error(err));
 }
